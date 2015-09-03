b5dcaba6d5540e3c974e35713015a885f1ad8605
vkornilov <fotokornilov@gmail.com>
2015-09-03 17:46:38 +0300

    fghfhfffff

---
+++ src/main/drfd.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by vkornilov on 03.09.2015.
+ */
+public class drfd {
+    public static void main(String[] args) {
+        System.out.println("s ds dfd");
+    }
+}
